929f1ed556d1caa6f7d3967a979055363657574d
Kerem-Kaplan <keremkaplan1000@gmail.com>
2022-07-14 18:16:16 +0300

    Yazılım Geliştirici Kampı 3.Gün

--- DegerVeReferansTipler/Program.cs
+++ DegerVeReferansTipler/Program.cs
@@ -19,6 +19,8 @@ namespace DegerVeReferansTipler
             int[] sayilar2 = new int[] { 100, 200, 300 };
             sayilar1 = sayilar2;
             sayilar2[0] = 999;
+
+
             //sayilar1[0] değeri 999 olur
             
             //int, decimal, float, double, bool --> değer tip ==> bellekteki 'stack' alanında tutulur ve işlemler gerceklesir
