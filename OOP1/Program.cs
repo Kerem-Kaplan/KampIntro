809264eeb956c9dc81c232b09b9c6e2dc2a34df8
Kerem-Kaplan <keremkaplan1000@gmail.com>
2022-07-16 19:10:20 +0300

    KampIntro

---
+++ OOP1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.ID = 1;
+            product1.CategoryID = 2;
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 500;
+            product1.UnitsInStock = 3;
+
+            Product product2 = new Product { ID = 2, CategoryID = 5, UnitsInStock = 5, ProductName = "Kalem", UnitPrice = 35 };
+
+            //case sensitive
+            //PascalCase   //camelCase
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+            Console.WriteLine(product1.ProductName);
+
+
+            //int, double, bool... deger tip
+            //diziler, class, abstract class, inretface... referans tip
+            //ref out
+
+        }
+    }
+}
